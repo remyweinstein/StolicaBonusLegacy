ea6c32037144b68cf96782c71d8c6c35bf05d567
Remy Weinstein <d.grebenyuk30@gmail.com>
2022-09-21 09:57:01 +1000

    downgrade version

--- app/php/version.php
+++ app/php/version.php
@@ -1,4 +1,4 @@
 <?php
-    define("APP_VERSION", "APP_232");
-    define("APP_VERSION_ANDROID", "232");
+    define("APP_VERSION", "APP_231");
+    define("APP_VERSION_ANDROID", "231");
     define("APP_VERSION_IOS", "225");

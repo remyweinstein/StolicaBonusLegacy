ec412003beb9ae0a94b823ded04edf3f81fb623e
Remy Weinstein <d.grebenyuk30@gmail.com>
2022-10-14 09:19:14 +1000

    Подняли версию до 238

--- app/php/version.php
+++ app/php/version.php
@@ -1,4 +1,4 @@
 <?php
-    define("APP_VERSION", "APP_231");
-    define("APP_VERSION_ANDROID", "233");
-    define("APP_VERSION_IOS", "225");
+    define("APP_VERSION", "APP_238");
+    define("APP_VERSION_ANDROID", "238");
+    define("APP_VERSION_IOS", "238");
